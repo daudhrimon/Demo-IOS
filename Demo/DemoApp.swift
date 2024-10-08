b1595a86fddde77bb9ae1fe5a9a7eacaee33467e
daudhrimon <daudhrimon@gmail.com>
2024-10-08 09:14:31 +0600

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Daud Hoshen on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
